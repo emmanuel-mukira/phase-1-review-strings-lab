82eb8028d32f4d7600e0a30908df8e0c86738872
Emmanuel <emmanuel.mukira@student.moringaschool.com>
2023-03-13 10:52:46 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code in this file!
+let currentUser="Emmanuel";
+let welcomeMessage=`Welcome to Flatbook, ${currentUser}!`;
+let excitedWelcomeMessage=`WELCOME TO FLATBOOK, EMMANUEL!`;
+let shortGreeting="Welcome, E !"
